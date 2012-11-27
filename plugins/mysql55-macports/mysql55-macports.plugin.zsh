6be03e1fd509a108d82a6c65eaaa920741d88269
Juan Francisco Rebolledo Gómez <juanrebolledog@gmail.com>
2012-11-27 08:32:06 -0430

    mysql55-macports plugin. Uses the mysql55 package from macports which has different file paths for mysql.server

---
+++ plugins/mysql55-macports/mysql55-macports.plugin.zsh
@@ -0,0 +1,8 @@
+# commands to control local mysql-server installation
+# paths are for osx installation via macports
+
+alias mysqlstart='sudo /opt/local/share/mysql55/support-files/mysql.server start'
+alias mysqlstop='sudo /opt/local/share/mysql55/support-files/mysql.server stop'
+alias mysqlrestart='sudo /opt/local/share/mysql55/support-files/mysql.server restart'
+
+alias mysqlstatus='mysqladmin5 -u root -p ping'
